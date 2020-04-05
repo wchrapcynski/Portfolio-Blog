d7f42b5a8cbe141cce6b479499786cb5c74c3773
William Chrapcynski <wchrapcynski@gmail.com>
2020-04-04 20:30:59 -0400

    Updated models and created views

--- portfolio_blog/blog/views.py
+++ portfolio_blog/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import (render, get_object_or_404, redirect)
+from django.url import reverse_lazy
+from django.utils import timezone
+from blog.models import Post, Comment
+from django.views.generic import(TemplateView, ListView, DetailView)
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Posts.objects.filter(
+            published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
