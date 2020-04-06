70145231262ebc3d3a29120e617c7748c1969c1b
William Chrapcynski <wchrapcynski@gmail.com>
2020-04-06 17:24:01 -0400

    Set up of initial files

---
+++ portfolio_blog_front/src/components/blogDetail/blogDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'blogDetail.scss'
+
+function BlogDetail() {
+  return(
+    <div></div>
+  )
+}
+
+export default BlogDetail
---
+++ portfolio_blog_front/src/components/blogList/blogList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './blogList.scss';
+
+function BlogList() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BlogList
