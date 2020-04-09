79095b5e7ad7209abea055c7babc2a30f2f5915b
William Chrapcynski <wchrapcynski@gmail.com>
2020-04-09 14:46:01 -0400

    Made adjustments to Django admin and how things are displayed

--- portfolio_blog_api/blog/admin.py
+++ portfolio_blog_api/blog/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'published', 'text']
+    search_fields = ['title']
+    list_filter = ['published']
+    list_display = ['title', 'published']
+    list_editable = ['published']
+admin.site.register(Post, PostAdmin)
